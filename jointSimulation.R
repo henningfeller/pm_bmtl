800e8524e5d45987e36878a3bde52cff9299af5e
kmalit <38551683+kmalit@users.noreply.github.com>
2018-05-30 17:07:27 +0200

    Update jointSimulation.R

--- jointSimulation.R
+++ jointSimulation.R
@@ -1,18 +1,42 @@
-rm(ls = list())
+rm(list = ls())
 graphics.off()
+
+library(dplyr)
 
 source("inputSimulation.R")
 source("greekSimulation.R")
 
-inputData <- simulation_of_input_data(2000)
 
-model.parameters <- simulate_model_parameters(inputData,
-																							inputVariables = colnames(inputData[2:6]),
-																							targetVariables = c("STK", "AMI", "ARF") )
+n = 2000 #Number of patients
+
+inputData <- simulation_of_input_data(n)
+inputData <- inputData %>% mutate(gender = ifelse(gender == 1, 'm','w')) # I needed to change this so the standardization function doesn't treat is as numeric variable
+
+
+
+## STANDARDIZING CONTINUOUS VARIABLES AND ONEHOT ENCODING FOR FACTOR VARIABLES:
+
+#Since the one hot encoding function takes non numeric variables and converts them to numeric variables, we want to perform this
+#change after standardizing the numeric variables otherwise the standardization function would take the one hot encoded variables and
+#standardize them which we do not want
+
+# @Henning, you are free to change the standardization here. I used N(0,1) as that's what most of the literature I read was pointing to
+
+stdInputData <- standardize_mean0_var1(inputData)
+stdInputData <- one_hot_encoding(stdInputData)
+
+
+## Applyting the standardized data to the simulated parameters to produce Ys:
+inputVariables = colnames(stdInputData)[-1];
+targetVariables = c("STK", "AMI", "ARF")
+model.parameters <- simulate_model_parameters(stdInputData,
+																							inputVariables,
+																							targetVariables )
 
 alpha <- model.parameters[["alpha"]]
 beta <- model.parameters[["beta"]]
 
+targetVariables.n <- length(targetVariables) 
 
 logit.theta <- matrix(NA, nrow = n, ncol = targetVariables.n)
 theta <- matrix(NA, nrow = n, ncol = targetVariables.n)
@@ -20,11 +44,9 @@ y <- matrix(NA, nrow = n, ncol = targetVariables.n)
 
 for (j in 1:n) { # patient
 	for (k in 1:targetVariables.n) { # outcome
-		logit.theta[j,k]<- alpha[k] + beta[,k] %*% as.numeric(inputData[j,2:6])
+		logit.theta[j,k]<- alpha[k] + beta[,k] %*% as.numeric(stdInputData[j,-1])
 		theta[j,k] <- 1/(1+exp(-logit.theta[j,k]))
 		y[j,k] <- rbern(1, theta[j,k])
 	}
 }
-
-
 
